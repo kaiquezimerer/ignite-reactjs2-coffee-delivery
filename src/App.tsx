035a3ca621bd2dfd6e430100cb21a1281281e53e
Kaíque Zimerer <kaiquezimerer@outlook.com>
2023-01-17 14:49:48 -0300

    Config. default theme colors + global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
-  return <p>Coffee Delivery</p>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <p>Coffee Delivery</p>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default App
---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${(props) => props.theme['gray-100']};
+    color: ${(props) => props.theme['brown-900']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`

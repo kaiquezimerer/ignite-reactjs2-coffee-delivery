a66cc9e995d81bcad2383487067fc6f53b2b9868
Kaíque Zimerer <kaiquezimerer@outlook.com>
2023-01-17 16:45:06 -0300

    Add links to Header navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { MapPin } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 
 import { HeaderContainer, Container, Navigation, LocationLabel } from './styles'
 
@@ -10,23 +11,27 @@ export function Header() {
   return (
     <HeaderContainer>
       <Container>
-        <h1>
-          <img
-            src={CoffeeDeliveryLogo}
-            alt="Coffee Delivery"
-            title="Coffee Delivery"
-          />
-        </h1>
+        <NavLink to="/">
+          <h1>
+            <img
+              src={CoffeeDeliveryLogo}
+              alt="Coffee Delivery"
+              title="Coffee Delivery"
+            />
+          </h1>
+        </NavLink>
         <Navigation>
           <ul>
             <li>
               <LocationLabel>
                 <MapPin size={22} weight="fill" />
-                Porto Alegre, RS
+                São Paulo, SP
               </LocationLabel>
             </li>
             <li>
-              <CartButton count={0} />
+              <NavLink to="/checkout">
+                <CartButton count={0} />
+              </NavLink>
             </li>
           </ul>
         </Navigation>
